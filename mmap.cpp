2020fc4a5c3d8a0bfbab2a844c291f6d928d4c49
ZyklonR <92772530+ZyklonR@users.noreply.github.com>
2022-05-16 22:19:58 -0700

    Update mmap.cpp

--- mmap.cpp
+++ mmap.cpp
@@ -17,24 +17,46 @@ cout << "FILE NAME missing\n";
 exit(1);
 }
 /* Open the specified file */
-int fd = open(argv[1], O_RDWR);
+int openfd = open(argv[1], O_RDWR);
+int closefd = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 
-if (fd < 0)
+if (openfd < 0)
 {
 cout << "\n" << "input file cannot be opened" << "\n";
 exit(1);
 }
+if (closefd < 0)
+{
+cout << "\n" << "output file cannot be opened" << "\n";
+exit(1);
+}
+
+
 struct stat stats;
 if (stat(argv[1], &stats) == 0)
 cout << endl << "file size " << stats.st_size;
 else
 cout << "Unable to get file properties.\n";
 /* Get the page size  */
+int fileSize = stats.st_size;
 int pagesize = getpagesize();
 cout << endl << "page size is " << pagesize << "\n";
 /* map the file into memory */
+
+  lseek(outFd, filesize - 1, SEEK_SET);
+  lseek(outFd, 0, SEEK_SET);
+
+while(fileSize > 0){
+    if(fileSize < pagesize){
+        pagesize = fileSize;
+        fileSize = 0;
+    }else
+    fileSize -= pagesize;
+}
+
+
 char* data = (char*)mmap(NULL, pagesize, PROT_READ | PROT_WRITE, MAP_SHARED, 
-fd, 0);
+openfd, 0);
 /* Did the mapping succeed ? */
 if (!data)
 {
@@ -48,13 +70,25 @@ cout << data[fIndex];
 }
 cout << endl;
 /* Write a string to the mapped region */
+char* mapped =(char*)mmap(NULL, pagesize,PROT_READ | PROT_WRITE | MAP_SHARED, closefd, 0);
+if (!mapped){
+    cout <<"\n" << "mapping did not succeed" << "\n";
+    exit(1);
+}
+
 /* memcpy(data, "Hello world, this is a test\n", sizeof("Hello world, this is a 
 test"));*/
 /* Unmap the shared memory region */
+memcpy(mapped, data, pagesize);
+munmap(mapped, pagesize);
 munmap(data, pagesize);
+lseek(openfd, pagesize, SEEK_SET);
+lseek(closefd, pagesize, SEEK_SET)
+
+
 /* Close the file */
-close(infd);
-close(outfd);
+close(openfd);
+close(closefd);
 
 return 0;
 }
